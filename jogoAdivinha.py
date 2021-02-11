735e723e05b8b18086bedb267ab61a094912cb68
smachado <samuelmachado1@gmail.com>
2021-02-11 12:29:15 -0300

    feat(game): variable rename and to parse int kick

--- jogoAdivinha.py
+++ jogoAdivinha.py
@@ -2,13 +2,15 @@ print("**********************************")
 print(" Bem vindo ao jogo da Adivinhação!")
 print("**********************************")
 
-numero_secreto = 42
+secret_number = 42
 
-chute = input("Digite o seu número: ")
+kick_str = input("Digite o seu número: ")
 
-print("Você digitou ", chute)
+print("Você digitou ", kick_str)
 
-if(numero_secreto == chute):
+kick = int(kick_str)
+
+if(secret_number == kick):
     print("Você advinhou o número secreto")
 else:
     print("Você errou!")
