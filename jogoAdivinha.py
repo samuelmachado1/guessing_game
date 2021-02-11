f5884642eaf477be705408c465b4d322795dfb6b
smachado <samuelmachado1@gmail.com>
2021-02-11 13:06:18 -0300

    feat(game): Improving game logic

--- jogoAdivinha.py
+++ jogoAdivinha.py
@@ -3,14 +3,28 @@ print(" Bem vindo ao jogo da Adivinhação!")
 print("**********************************")
 
 secret_number = 42
+attempts_total = 3
+round_attempts = 1
 
-kick_str = input("Digite o seu número: ")
+while(round_attempts <= attempts_total):
+    print("Tentativa ", round_attempts, " de ", attempts_total )
+    kick_str = input("Digite o seu número: ")
 
-print("Você digitou ", kick_str)
+    print("Você digitou ", kick_str)
 
-kick = int(kick_str)
+    kick = int(kick_str)
 
-if(secret_number == kick):
-    print("Você advinhou o número secreto")
-else:
-    print("Você errou!")
+    hit = kick == secret_number 
+    its_bigger = kick > secret_number
+    its_smaller = kick < secret_number
+
+    if(hit):
+        print("Você advinhou o número secreto")
+    else:
+        if(its_bigger):
+            print("Você errou! O número que você digitou é maior que o número secreto.")
+        elif(its_smaller):
+            print("Você errou! O número que você digitou é menor que o número secreto.")
+
+    round_attempts = round_attempts + 1
+print("Fim do jogo")
